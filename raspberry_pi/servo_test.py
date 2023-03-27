56ecb916e3fbc6d79382c61547ecf6666645fdbd
Imad Ahmad <99471806+imadahmad97@users.noreply.github.com>
2023-03-26 18:56:07 -0600

    added   servo test file

---
+++ raspberry_pi/servo_test.py
@@ -0,0 +1,39 @@
+import RPi.GPIO as GPIO
+import time
+
+
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(11,GPIO.OUT)
+servo = GPIO.PWM(11,50)
+
+
+if model_output == 1:
+    servo.start(0)
+    print("Waiting for 1 second")
+    time.sleep(1)
+
+    print("Rotating from 0 to 90 degrees")
+    duty = 2
+    while duty <= 7:
+        servo.ChangeDutyCycle(duty)
+        time.sleep(0.05)
+        duty = duty + 1
+
+    print("Waiting for 1 second")
+    time.sleep(10)
+
+    print("Rotating from 90 to 0 degrees")
+    duty = 7
+    while duty >= 2:
+        servo.ChangeDutyCycle(duty)
+        time.sleep(0.05)
+        duty = duty - 1
+
+    print("Turning back to 0 degrees")
+    servo.ChangeDutyCycle(0)
+    time.sleep(1)
+    servo.stop()
+    GPIO.cleanup()
+    print("Done and Dusted")
